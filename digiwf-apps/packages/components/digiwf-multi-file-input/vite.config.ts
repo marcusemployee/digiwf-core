eef5229bc8353e883f3464768bd501b638ca32b0
Dominik Horn <dominik.horn93@gmail.com>
2022-08-30 13:55:13 +0200

    Feat/add apps
    * feat: add frontend apps
    
    * feat: add apps
    
    * feat: add digiwf-api
    
    * fix: init file input migration
    
    * feat: add multi file input
    
    * chore: build pipelines
    
    * chore: build apps
    
    * fix: cache dependency path

---
+++ digiwf-apps/packages/components/digiwf-multi-file-input/vite.config.ts
@@ -0,0 +1,47 @@
+import {createVuePlugin} from "vite-plugin-vue2";
+import {defineConfig} from 'vite'
+//@ts-ignore
+import {fileURLToPath, URL} from "url";
+import Components from 'unplugin-vue-components/vite'
+import {VuetifyResolver} from "unplugin-vue-components/resolvers";
+
+export default defineConfig({
+    plugins: [
+        createVuePlugin(),
+        Components({
+            transformer: 'vue2',
+            dts: true,
+            resolvers: [
+                VuetifyResolver()
+            ]
+        })
+    ],
+    build: {
+        target: 'esnext',
+        lib: {
+            entry: 'src/index.ts',
+            name: 'digiwf-multi-file-input',
+        },
+        rollupOptions: {
+            external: [
+                'vue',
+                'vuex',
+                'vue-router',
+                /@vuetify\/.*/,
+                /@vue\/.*/
+            ],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        },
+        minify: 'esbuild'
+    },
+    resolve: {
+        alias: {
+            //@ts-ignore
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+})
