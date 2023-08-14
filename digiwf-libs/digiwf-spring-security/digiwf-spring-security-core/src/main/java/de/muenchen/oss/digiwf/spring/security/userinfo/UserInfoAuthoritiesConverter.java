bcaafdb327b2a731e7f5e6d8de8d021feb0f222d
Dominik Horn <dominik.horn93@gmail.com>
2023-08-14 10:16:55 +0200

    Feature/secure cockpit + sso roles (#575)
    * implementing secure cockpit
    
    * implement secure cockpit via gateway
    
    * #402: add camunda plugin filter rule
    
    * integration of groups
    
    * fixed review comments
    
    ---------
    
    Co-authored-by: Simon Zambrovski <simon.zambrovski@holisticon.de>
    Co-authored-by: stephan.strehler <stephan.strehler@cgi.com>

--- digiwf-libs/digiwf-spring-security/digiwf-spring-security-core/src/main/java/de/muenchen/oss/digiwf/spring/security/userinfo/UserInfoAuthoritiesConverter.java
+++ digiwf-libs/digiwf-spring-security/digiwf-spring-security-core/src/main/java/de/muenchen/oss/digiwf/spring/security/userinfo/UserInfoAuthoritiesConverter.java
@@ -1,13 +1,16 @@
-package de.muenchen.oss.digiwf.spring.security;
+package de.muenchen.oss.digiwf.spring.security.userinfo;
 
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Ticker;
+import de.muenchen.oss.digiwf.spring.security.JwtClaims;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.Cache;
 import org.springframework.cache.Cache.ValueWrapper;
 import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -27,16 +30,13 @@ import java.util.stream.Stream;
 
 /**
  * Service, der einen OIDC /userinfo Endpoint aufruft (mit JWT Bearer Auth) und dort die enthaltenen
- * "Authorities" und "User_Roles extrahiert.
+ * "authorities" und "user_roles" extrahiert.
  */
 @Slf4j
-public class UserInfoAuthoritiesService {
+public class UserInfoAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
   private static final String NAME_AUTHENTICATION_CACHE = "authentication_cache";
   private static final int AUTHENTICATION_CACHE_ENTRY_SECONDS_TO_EXPIRE = 60;
-
-  private static final String CLAIM_AUTHORITIES = "authorities";
-  private static final String CLAIM_ROLES = "user_roles";
 
   private final String userInfoUri;
   private final RestTemplate restTemplate;
@@ -48,7 +48,7 @@ public class UserInfoAuthoritiesService {
    * @param userInfoUri         userinfo Endpoint URI
    * @param restTemplateBuilder ein {@link RestTemplateBuilder}
    */
-  public UserInfoAuthoritiesService(final String userInfoUri, final RestTemplateBuilder restTemplateBuilder) {
+  public UserInfoAuthoritiesConverter(final String userInfoUri, final RestTemplateBuilder restTemplateBuilder) {
     this.userInfoUri = userInfoUri;
     this.restTemplate = restTemplateBuilder.build();
     this.cache = new CaffeineCache(NAME_AUTHENTICATION_CACHE,
@@ -58,18 +58,24 @@ public class UserInfoAuthoritiesService {
             .build());
   }
 
+  @Override
+  public Collection<GrantedAuthority> convert(@NonNull Jwt source) {
+    return loadAuthorities(source);
+  }
+
+
   /**
    * Ruft den /userinfo Endpoint und extrahiert {@link GrantedAuthority}s aus dem "authorities"
-   * Claim.
+   * Claim und roles aus dem "user_roles" claim.
    *
    * @param jwt der JWT
    * @return die {@link GrantedAuthority}s gem. Claim "authorities" des /userinfo Endpoints
    */
-  public Collection<SimpleGrantedAuthority> loadAuthorities(final Jwt jwt) {
+  public Collection<GrantedAuthority> loadAuthorities(final Jwt jwt) {
     final ValueWrapper valueWrapper = this.cache.get(jwt.getSubject());
     if (valueWrapper != null) {
       // value present in cache
-      @SuppressWarnings("unchecked") final Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>) valueWrapper.get();
+      @SuppressWarnings("unchecked") final Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) valueWrapper.get();
       log.debug("Resolved authorities (from cache): {}", authorities);
       return authorities;
     }
@@ -78,32 +84,28 @@ public class UserInfoAuthoritiesService {
     final HttpHeaders headers = new HttpHeaders();
     headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + jwt.getTokenValue());
     final HttpEntity<String> entity = new HttpEntity<>(headers);
-
-    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+    Collection<GrantedAuthority> authorities = new ArrayList<>();
     try {
-      @SuppressWarnings("unchecked") final Map<String, Object> map = this.restTemplate.exchange(this.userInfoUri, HttpMethod.GET, entity,
-          Map.class).getBody();
-
+      @SuppressWarnings("unchecked")
+      final Map<String, Object> map = this.restTemplate.exchange(this.userInfoUri, HttpMethod.GET, entity, Map.class).getBody();
       log.debug("Response from user-info Endpoint: {}", map);
-      if (map != null && map.containsKey(CLAIM_AUTHORITIES)) {
-        authorities.addAll(asAuthorities(map.get(CLAIM_AUTHORITIES)));
+      if (map != null && map.containsKey(JwtClaims.AUTHORITIES)) {
+        authorities.addAll(asAuthorities(map.get(JwtClaims.AUTHORITIES)));
       }
-      if (map != null && map.containsKey(CLAIM_ROLES)) {
-        authorities.addAll(asAuthorities(map.get(CLAIM_ROLES)));
+      if (map != null && map.containsKey(JwtClaims.ROLES)) {
+        authorities.addAll(asAuthorities(map.get(JwtClaims.ROLES)));
       }
       log.debug("Resolved Authorities (from /userinfo Endpoint): {}", authorities);
       // store
       this.cache.put(jwt.getSubject(), authorities);
     } catch (final Exception e) {
-      log.error(String.format("Could not fetch user details from %s - user is granted NO authorities",
-          this.userInfoUri), e);
+      log.error(String.format("Could not fetch user details from %s - user is granted NO authorities", this.userInfoUri), e);
     }
-
     return authorities;
   }
 
-  private static List<SimpleGrantedAuthority> asAuthorities(Object object) {
-    final List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+  private static List<GrantedAuthority> asAuthorities(Object object) {
+    final List<GrantedAuthority> authorities = new ArrayList<>();
     if (object instanceof Collection) {
       final Collection<?> collection = (Collection<?>) object;
       object = collection.toArray(new Object[0]);
